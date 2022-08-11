437ae1ae186cecaabdfb5d01a0b2300bf73b82df
MHutatah <hutatah@gmail.com>
2022-08-11 21:05:05 +0300

    fixed sql script

--- postgress.sh
+++ postgress.sh
@@ -1,10 +1,9 @@
 docker run -d \
 --name postgresDB \
+--network=backend \
 -e POSTGRES_USER=demo_user \
 -e POSTGRES_PASSWORD=demo_user \
 -e POSTGRES_DB=demo_db \
--v /home/ubuntu/config:/etc/postgresql/11.2/main directory \
--v /home/ubuntu/data:/var/lib/postgresql/data \
--v /home/ubuntu/yourfile/sql_scripts/:/docker-entrypoint-initdb.d \
-
+-v /home/ubuntu/{choose-a-directory}:/var/lib/postgresql/data \
+-v /home/ubuntu/{your-repo}/sql_scripts/:/docker-entrypoint-initdb.d \
 postgres:11.2-alpine
--- sql_scripts/init.sql
+++ sql_scripts/init.sql
@@ -1,5 +1,4 @@
 create table english2arabic ( english text, arabic text);
-
 insert into english2arabic(english, arabic) values ('Good morning', 'Sabah Alkhar');
 insert into english2arabic(english, arabic) values ('It was fun', 'kan mumtiean');
 insert into english2arabic(english, arabic) values ('Yes', 'Naam');
