181d6cbfafc4cfc60a4e915d2f32583dfb3e6991
MHutatah <hutatah@gmail.com>
2022-08-11 20:09:12 +0300

    fixed data mount

--- postgress.sh
+++ postgress.sh
@@ -3,8 +3,8 @@ docker run -d \
 -e POSTGRES_USER=demo_user \
 -e POSTGRES_PASSWORD=demo_user \
 -e POSTGRES_DB=demo_db \
--v /home/ubuntu/config:/docker-entrypoint-initdb.d \
--v /home/ubuntu/data:/etc/postgresql/11.2/main directory \
--v -v /home/ubuntu/yourfile/sql_scripts/:/docker-entrypoint-initdb.d \
+-v /home/ubuntu/config:/etc/postgresql/11.2/main directory \
+-v /home/ubuntu/data:/var/lib/postgresql/data \
+-v /home/ubuntu/yourfile/sql_scripts/:/docker-entrypoint-initdb.d \
 
 postgres:11.2-alpine
