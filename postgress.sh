b40976dad1cf0eeae8ec04b4303ff5448d02b8ee
MHutatah <hutatah@gmail.com>
2022-08-11 23:54:46 +0300

    final fixes

--- postgress.sh
+++ postgress.sh
@@ -4,6 +4,6 @@ docker run -d \
 -e POSTGRES_USER=demo_user \
 -e POSTGRES_PASSWORD=demo_user \
 -e POSTGRES_DB=demo_db \
--v /home/ubuntu/{choose-a-directory}:/var/lib/postgresql/data \
--v /home/ubuntu/{your-repo}/sql_scripts/:/docker-entrypoint-initdb.d \
+-v /home/ubuntu/data:/var/lib/postgresql/data \
+-v /home/ubuntu/Belt2Day5/sql_scripts/:/docker-entrypoint-initdb.d \
 postgres:11.2-alpine
